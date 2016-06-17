3b962cc4ffd61b7f8f39a927936dcb7d47c6be65
Philip Lombardi <plombardi@datawire.io>
2016-06-17 13:04:22 -0400

    Working version that talks to downstreams

---
+++ README.md
@@ -0,0 +1,2 @@
+# Datawire Microcosm
+
--- microcosm.py
+++ microcosm.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import requests
+import sys
 
 from discovery import Discovery, Node
 from datawire_introspection import Platform, DatawireToken
@@ -13,6 +14,16 @@ discovery = Discovery()
 
 app = Flask(__name__)
 
+NODE_REGISTERED              = "*** node started        (service: %s, version: %s)"
+RECV_REQUEST_MSG             = "--> request             (id: %s)"
+RECV_DOWNSTREAM_RESPONSE_MSG = "--> downstream response (id: %s)"
+SENT_RESPONSE_MSG            = "<-- response            (id: %s)"
+
+
+# TODO: Remove once Discovery client can resolve versions properly.
+def create_service_name(name, version):
+    return "{}:{}".format(name, version)
+
 
 def is_foundational():
     return len(config.get('dependencies', [])) == 0
@@ -21,28 +32,36 @@ def is_foundational():
 @app.route('/', methods=['POST'])
 def process_request():
     request_id = str(uuid4())
-    app.logger.info("RECV REQUEST (id: {})", )
+    app.logger.info(RECV_REQUEST_MSG, request_id)
     result = []
 
     if is_foundational():
         result.append({'request_id': request_id})
     else:
         for service in config.get('dependencies'):
-            address = discovery.resolve(service)
+            address = discovery.resolve(service).address
             response = requests.post('http://{}/'.format(address))
-            app.logger.info("RECV RESPONSE")
-            result.append(response.json)
+            responder_data = response.json()
+            responder_node = responder_data[0]
+            app.logger.info(RECV_DOWNSTREAM_RESPONSE_MSG, responder_node['request_id'])
+            result = result + responder_data
 
-    app.logger.info("SENT RESPONSE")
+    app.logger.info(SENT_RESPONSE_MSG, request_id)
     return jsonify(result)
 
 
 if __name__ == '__main__':
     app.debug = False
 
-    import json
+    import logging
+    for logger_name in ['werkzeug', 'quark.discovery', 'requests', 'urllib3']:
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(logging.ERROR)
 
-    with open('microcosm.json') as config_file:
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
+
+    import json
+    with open(sys.argv[1]) as config_file:
         config = json.load(config_file)
 
     service_host = Platform.getRoutableHost()
@@ -54,17 +73,16 @@ if __name__ == '__main__':
     discovery.connectTo(datawire_config.get('discoveryAddress'))
 
     node = Node()
-    node.service = config.get('service')
+    # Hack to work around lack of version support in the client. Make the name <service>:<version>
+    service_name = config.get('service')
+    service_version = config.get('version')
+
+    node.service = create_service_name(service_name, service_version)
     node.address = '{}:{}'.format(service_host, service_port)
     node.version = config.get('version')
 
     discovery.start()
     discovery.register(node)
-
-    import logging
-    werkzeug_logger = logging.getLogger('werkzeug')
-    werkzeug_logger.setLevel(logging.ERROR)
-
-    logging.basicConfig(level=logging.INFO)
+    app.logger.info(NODE_REGISTERED, service_name, service_version)
 
     app.run(host=config.get('host', '0.0.0.0'), port=config.get('port'))
